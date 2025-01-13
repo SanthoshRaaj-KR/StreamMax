1a16c306b79855fcf90af9b70a184a776d21db62
Santhosh <santhoshraaj1710@gmail.com>
2025-01-13 12:37:17 +0530

    Second commit with OTT API include

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ import express from 'express';
 import axios from 'axios';
 import qs from 'qs';
 import path from 'path';
+import { start } from 'repl';
 
 const port = 3000;
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
-})
+app.get("/", async (req,res)=>{
+  try{
+    const result=await axios.get("https://ott-details.p.rapidapi.com/advancedsearch",{params:{
+      start_year:'2000',
+      min_imdb:'6',
+      type:'movie',
+      language:'english',
+    },
+    headers:{
+      'x-rapidapi-key': '63fa050d33mshbc7ee4ca45209a4p1e7207jsn4a92ba421bdd',
+      'x-rapidapi-host': 'ott-details.p.rapidapi.com'
+    }
+    });
+    res.render("index.ejs",{content:result.data});
+  }
+  catch(error){
+    console.log(error.message);
+    res.status(500).send("Something went wrong while fetching data.");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Running at port ${port}`);
